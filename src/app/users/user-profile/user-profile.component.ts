9db3806bdb7f8ec46be8d1857814526fc5ad25f3
radichev <unbreaking78@gmail.com>
2020-06-22 00:33:59 +0300

    Added loading spinner and check for the data

--- src/app/users/user-profile/user-profile.component.ts
+++ src/app/users/user-profile/user-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../shared/user.service';
 import { UserProfile } from '../shared/models/user-profile';
 import { ActivatedRoute } from '@angular/router';
+import { JwtService } from 'src/app/shared/jwt.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,13 +11,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserProfileComponent implements OnInit {
 
+  isLoading = true;
   id: string;
+  username: string;
   userProfile: UserProfile;
 
-  constructor(private userService: UserService, private route: ActivatedRoute) { }
+  constructor(private userService: UserService, private route: ActivatedRoute, private jwtService:JwtService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
-    this.userService.getUserProfile(this.id).subscribe(data => { this.userProfile = data });
+    this.username = this.jwtService.getUsername();
+    this.userService.getUserProfile(this.id).subscribe(data => { 
+    this.userProfile = data;
+    this.isLoading = false;
+    });
   }
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from '../shared/guards/auth.guard';
 import { UserService } from './shared/user.service';
 import {MatCardModule} from '@angular/material/card';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatProgressSpinnerModule, MatSpinner} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [UserProfileComponent],
@@ -19,7 +20,8 @@ import {MatExpansionModule} from '@angular/material/expansion';
     RouterModule,
     UsersRoutingModule,
     MatCardModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatProgressSpinnerModule
   ]
 })
 export class UsersModule { }
