cabd76b863b90d43f58e8441c1462e7ccb5e448c
radichev <unbreaking78@gmail.com>
2020-07-08 22:59:30 +0300

    Added FormControls

--- src/app/users/user-profile-edit/user-profile-edit.component.ts
+++ src/app/users/user-profile-edit/user-profile-edit.component.ts
@@ -3,9 +3,7 @@ import { UserProfile } from '../shared/models/user-profile';
 import { UserService } from '../shared/user.service';
 import { ActivatedRoute } from '@angular/router';
 import { JwtService } from 'src/app/shared/jwt.service';
-import { LanguageLevel } from '../shared/models/language-models/language-level';
-import { SkillLevel } from '../shared/models/skill-models/skill-level';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { LookupTables } from '../shared/models/lookup-tables';
 
 @Component({
@@ -28,7 +26,7 @@ export class UserProfileEditComponent implements OnInit {
   educationsForm: FormGroup;
   certificateForm: FormGroup;
 
-  constructor(private userService: UserService, private route: ActivatedRoute, private jwtService: JwtService) { }
+  constructor(private userService: UserService, private route: ActivatedRoute, private jwtService: JwtService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
@@ -43,7 +41,35 @@ export class UserProfileEditComponent implements OnInit {
     this.userService.getAllLookupTables().subscribe(data => {
       this.lookupTables = data;
       this.isLookupLoaded = false;
-    })
+    });
+
+    this.descriptionForm = this.formBuilder.group({
+      description: [null, [Validators.required]],
+    });
+
+    this.languageForm = this.formBuilder.group({
+      language: [null, [Validators.required]],
+      languageLevel: [null, [Validators.required]]
+    });
+
+    this.skillForm = this.formBuilder.group({
+      skill: [null, [Validators.required]],
+      skillLevel: [null, [Validators.required]]
+    });
+
+    this.educationsForm = this.formBuilder.group({
+      country: [null, [Validators.required]],
+      collegeName: [null, [Validators.required]],
+      titleType: [null, [Validators.required]],
+      major: [null, [Validators.required]],
+      graduationYear: [null, [Validators.required]]
+    });
+
+    this.certificateForm = this.formBuilder.group({
+      certificate: [null, [Validators.required]],
+      awardedFrom: [null, [Validators.required]],
+      graduationYear: [null, [Validators.required]]
+    });
   }
 
   editDescription() {
