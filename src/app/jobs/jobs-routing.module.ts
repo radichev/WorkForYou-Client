d5ec5e725894185836de6e33773eadf2975f6157
radichev <unbreaking78@gmail.com>
2020-07-16 00:13:54 +0300

    Added routing for jobs module

--- src/app/jobs/jobs-routing.module.ts
+++ src/app/jobs/jobs-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JobComponent } from './job/job.component';
+import { AuthGuard } from '../shared/guards/auth.guard';
+import { JobAddComponent } from './job-add/job-add.component';
 
 
 
 const routes: Routes = [
   {
-    path: '/:id'
+    path: '/job/:id',
+    component: JobComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '/job/add',
+    component: JobAddComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
