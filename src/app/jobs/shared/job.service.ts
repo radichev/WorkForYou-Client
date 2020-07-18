b5372e52c108f94a6df9de3140982ccc3a589308
radichev <unbreaking78@gmail.com>
2020-07-18 20:40:08 +0300

    Added get method for all work spheres and model

--- src/app/jobs/shared/job.service.ts
+++ src/app/jobs/shared/job.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { WorkSphereLookup } from './models/workSpheresLookup';
 
 @Injectable()
 export class JobService {
 
-  constructor() { }
+  private readonly WORK_SPHERES_LOOKUP_URL = `${environment.apiUrl}/work-spheres/all`
+
+  constructor(private http: HttpClient) { }
+
+  getWorkSpheres() {
+    return this.http.get<WorkSphereLookup>(`${this.WORK_SPHERES_LOOKUP_URL}`);
+  }
 }
---
+++ src/app/jobs/shared/models/workSpheresLookup.ts
@@ -0,0 +1,5 @@
+import { workSphere } from './work-spheres/work-sphere';
+
+export interface WorkSphereLookup {
+    workSpheres: [workSphere];
+}
