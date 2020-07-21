00940275d4cc0b1d16405f01ed3db0aeb18e67eb
radichev <unbreaking78@gmail.com>
2020-07-21 16:58:12 +0300

    Added HTML and CSS and user profile model to the job model

--- src/app/shared/models/input-models/job.ts
+++ src/app/shared/models/input-models/job.ts
@@ -1,13 +1,17 @@
 
 import { PictureModel } from './pictures-model/picture';
 import { workSphereInputModel } from './work-spheres/work-sphere';
+import { SubSphereInputModel } from './work-spheres/sub-sphere';
+import { UserProfileInputModel } from './user-profile/user-profile';
 
 export interface JobInputModel {
     id: string;
     jobTitle: string;
     workSphere: workSphereInputModel;
+    subSphere: SubSphereInputModel;
     deliveryTime: number;
     price: number;
     description: string;
     pictures: [PictureModel];
+    userProfileDetails: UserProfileInputModel;
 }
---
+++ src/app/shared/models/input-models/user-profile/user-profile.ts
@@ -0,0 +1,7 @@
+export interface UserProfileInputModel {
+    id: string;
+    description: string;
+    createdDate: string;
+    country: string;
+    username: string;
+}
